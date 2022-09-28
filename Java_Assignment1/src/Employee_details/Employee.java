65495656666049639cc4dcb9189b683ddc1573b1
DeepakSawalka <114117187+DeepakSawalka@users.noreply.github.com>
2022-09-27 23:03:32 -0400

    Add files via upload

---
+++ Java_Assignment1/src/Employee_details/Employee.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package employee_details;
+
+/**
+ *
+ * @author Deepak Sawalka
+ */
+public class Employee {
+    private String name;
+    private int emp_ID;
+    private int age;
+    private String gender;
+    private int date;
+    private int level;
+    private String team_info;
+    private String pos_title;
+    private int cell_number;
+    private String email_id;
+    private String img;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmp_ID() {
+        return emp_ID;
+    }
+
+    public void setEmp_ID(int emp_ID) {
+        this.emp_ID = emp_ID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getTeam_info() {
+        return team_info;
+    }
+
+    public void setTeam_info(String team_info) {
+        this.team_info = team_info;
+    }
+
+    public String getPos_title() {
+        return pos_title;
+    }
+
+    public void setPos_title(String pos_title) {
+        this.pos_title = pos_title;
+    }
+
+    public int getCell_number() {
+        return cell_number;
+    }
+
+    public void setCell_number(int cell_number) {
+        this.cell_number = cell_number;
+    }
+
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+    
+    
+    
+    
+    
+}
