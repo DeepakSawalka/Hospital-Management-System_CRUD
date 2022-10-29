0026cc0b26b2c09836c0359d098c34a137e12e95
DeepakSawalka <sawalka.d@northeastern.edu>
2022-10-29 19:17:59 -0400

    new commit

---
+++ Assign2/src/model/EncounterHistory.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Deepak
+ */
+public class EncounterHistory {
+    private ArrayList<Encounter>encounterHistory;
+
+    public EncounterHistory() {
+        this.encounterHistory = encounterHistory;
+    }
+
+    public ArrayList<Encounter> getEncounterHistory() {
+        return encounterHistory;
+    }
+
+    public void setEncounterHistory(ArrayList<Encounter> encounterHistory) {
+        this.encounterHistory = encounterHistory;
+    }
+    
+    /*public Encounter addNewEncounter {
+        Encounter newEncounter= new Encounter();
+        encounterHistory.add(newEncounter);
+        return newEncounter;
+    }
+    public void deleteEncounter(Encounter e){
+        encounterHistory.remove(e);
+        
+    }*/
+    
+    
+    
+}
